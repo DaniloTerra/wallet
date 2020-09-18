c92ff3d1b90fc18b8768fcedd19694a767589305
Danilo Terra <dterra@uolinc.com>
2020-09-17 21:40:34 -0300

    Adicionado testes da classe valor Money

--- tests/unit/Model/MoneyTest.php
+++ tests/unit/Model/MoneyTest.php
@@ -11,10 +11,53 @@ use Codeception\Test\Unit;
  */
 final class MoneyTest extends Unit
 {
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructWithPositiveValue()
+    {
+        $value = 100.00;
+
+        $amount = new Money($value);
+
+        static::assertInstanceOf(Money::class, $amount);
+    }
+
+
+    /**
+     * @covers ::__construct
+     */
     public function testConstructWithNegativeValueShouldThrowException()
     {
         static::expectException(\InvalidArgumentException::class);
 
         new Money(-100.00);
+    }
+
+
+    /**
+     * @covers ::increase
+     */
+    public function testIncreaseShouldReturnAnIncreasedInstance()
+    {
+        $amount = new Money(100.00);
+
+        $increased = $amount->increase(new Money(100.00));
+
+        static::assertNotSame($amount, $increased);
+        static::assertEquals(200.00, $increased->getValue());
+    }
+
+    /**
+     * @covers ::increase
+     */
+    public function testDecreaseShouldReturnADecreasedInstance()
+    {
+        $amount = new Money(200.00);
+
+        $increased = $amount->decrease(new Money(100.00));
+
+        static::assertNotSame($amount, $increased);
+        static::assertEquals(100.00, $increased->getValue());
     }
 }
