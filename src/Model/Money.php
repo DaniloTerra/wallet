22bcb0d81db3100f8dbbe323e849e94ac9b7420f
Danilo Terra <dterra@uolinc.com>
2020-09-17 21:40:10 -0300

    Adicionado classe de valor para representar valores monetários

--- src/Model/Money.php
+++ src/Model/Money.php
@@ -1,1 +1,34 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Wallet\Model;
+
+final class Money
+{
+    private float $value;
+
+    public function __construct(float $value)
+    {
+        if ($value <= 0.00) {
+            throw new \InvalidArgumentException(sprintf('Invalid value "%s" for %s object', $value, static::class));
+        }
+
+        $this->value = $value;
+    }
+
+    public function increase(self $amount): self
+    {
+        return new static($this->value + $amount->getValue());
+    }
+
+    public function decrease(self $amount): self
+    {
+        return new static($this->value - $amount->getValue());
+    }
+
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+}
